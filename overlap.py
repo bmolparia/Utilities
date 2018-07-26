4f03d0d951c37a06d896d2fe2b3492ac0c871746
Bhuvan Molparia <bhuvan@scripps.edu>
2018-07-26 16:02:04 -0700

    updates overlap

--- overlap.py
+++ overlap.py
@@ -6,42 +6,119 @@
 
 from sorted_collection import SortedCollection
 
+class Overlap(object):
+
+	def __init__(self, bin, overlapping_bins=None, overlap_amounts=None):
+
+		self.bin = bin
+		if overlapping_bins != None:
+			self._obin = overlapping_bins
+		else:
+			self._obin = []
+
+		if overlap_amounts != None:
+			self._oamts = overlap_amounts
+		else:
+			self._oamts = []
+
+	def __repr__(self):
+		''' Defines the way this obejct is printed '''
+		return '{} overlaps with:\n{}'.format(self.bin,
+		'\n'.join('{}:{}'.format(x,y) for x,y in zip(self._obin, self._oamts)))
+
+	@property
+	def overlapping_bins(self):
+		return self._obin
+
+	@overlapping_bins.getter
+	def overlapping_bins(self):
+		''' Defines the get attribute function for overlapping bins.'''
+		return list(zip(self._obin, self._oamts))
+
+	@overlapping_bins.setter
+	def overlapping_bins(self,value):
+		''' Defines the set attribute function for overlapping bins. It will
+		append any values that are being set to the original list '''
+
+		raise AttributeError('Attribute cannot directly be set')
+
+	def add_bin(self,bin,overlap_amt):
+		''' A method to add an overlapping bin to the list'''
+
+		self._obin.append(bin)
+		self._oamts.append(overlap_amt)
+
 def find_overlapping_bins(arr1, arr2):
 	''' Function to calculate overlaps for every bin in array 1 against
 	every bin in array 2. Accepts two arrays of tuples as inputs. Where each
-	tuple is the start and end site of a bin within that array'''
+	tuple is the start and end site of a bin within that array.
 
-	a1s,a1e = zip(*arr1)
-	a2s,a2e = zip(*arr2)
+	Returns: An Overlap object containing bins in arr1, and the indices of all
+	the bins in arr2 it overlaps with.
+	'''
+
+	arr2_sc = SortedCollection(arr2, key = (lambda x: x[0]))
 
 	overlapping_bins = []
-	i = 0
-	while i < len(a1s):
-		beg1 = a1s[i]
-		end1 = a1e[i]
-		seqL1 = float(end1-beg1+1)
-		le_val = SortedCollection(a2s).find_le(beg1)
-		indx = a2s.index(le_val)
+	for bin in arr1:
 
+		overlaps = Overlap(bin)
+		bin_beg, bin_end = bin
+
+		# Find the closest bin in array2
 		try:
-			beg2 = a2s[indx]
-			end2 = a2e[indx]
-			overlap = overlap_amount(beg1,end1,beg2,end2)
-			if overlap != 0.0:
-				overlapping_bins.append((beg1,end1,beg2,end2))
-		
-		except IndexError:
-			pass
-		i += 1
+			closest_bin = arr2_sc.find_le(bin_beg)
+			indx = arr2.index(closest_bin)
+		except ValueError:
+			closest_bin = arr2[0]
+			indx = 0
+
+		overlap_amt = get_overlap_amount(bin, closest_bin)
+		if overlap_amt > 0:
+			overlaps.add_bin(closest_bin, overlap_amt)
+
+			# Go Right
+			flag = True
+			c = 1
+			while flag:
+				try:
+					next_bin = arr2[indx+c]
+					overlap_amt = get_overlap_amount(bin, next_bin)
+					if overlap_amt > 0:
+						overlaps.add_bin(next_bin, overlap_amt)
+					else:
+						flag = False
+					c += 1
+				except IndexError:
+					flag = False
+
+			# Go Left
+			flag = True
+			c = -1
+			while flag:
+				try:
+					next_bin = arr2[indx+c]
+					overlap_amt = get_overlap_amount(bin, next_bin)
+					if overlap_amt > 0:
+						overlaps.add_bin(next_bin, overlap_amt)
+					else:
+						flag = False
+					c -= 1
+				except IndexError:
+					flag = False
+
+		overlapping_bins.append(overlaps)
 
 	return overlapping_bins
 
-def overlap_amount(s1,e1,s2,e2):
+def get_overlap_amount(bin1,bin2):
 	''' Given two bins, this function returns the amount points that overlap
 	between the two '''
 
-	seqL1 = float(e1-s1+1)
-	seqL2 = float(e2-s2+1)
+	s1,e1 = bin1
+	s2,e2 = bin2
+	seqL1 = e1-s1+1
+	seqL2 = e2-s2+1
 
 	if (s1<=s2) and (e1<e2) and (e1>=s2):
 		overlap = e1-s2+1
@@ -56,14 +133,17 @@ def overlap_amount(s1,e1,s2,e2):
 
 	return overlap
 
-
 if __name__ == "__main__":
 
-	arr1 = [(10,23),(41,62),(80,82),(106,125)]
-	arr2 = [(5,15),(55,85),(106,125)]
-	print(arr1,'\n')
+	arr1 = [(5,15),(55,85),(106,127)]
+	arr2 = [(10,23),(41,62),(80,82),(106,125)]
+
+	print(arr1)
 	print(arr2,'\n')
-
-
 	t = find_overlapping_bins(arr1, arr2)
-	print(t)
+	for i in t:
+		print(i.overlapping_bins)
+
+	# o = Overlap((10,30),[(5,14),(21,27),(30,33)],[5,7,1])
+	# print(o.overlapping_bins)
+	# o.overlapping_bins = []
